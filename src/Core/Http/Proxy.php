1da9dd4e27c7408db78ace0ae56d93c4b2570a05
hughsaffar <hughsaffar@gmail.com>
2016-05-24 01:53:43 -0400

    Proxy Type (#11)
    * Add proxy type argument into the Proxy constructor
    
    * Add type argument into Proxy constructor
    
    * Add getter for Type
    
    * Fix: Proxy Type

--- src/Core/Http/Proxy.php
+++ src/Core/Http/Proxy.php
@@ -12,17 +12,21 @@ class Proxy implements ProxyInterface
 
     protected $ip;
     protected $port;
+    protected $type;
+    protected $type; // Proxy Types: HTTP, SOCKS4 and SOCKS5
     protected $user;
     protected $password;
     protected $scheme;
 
-    public function __construct($ip, $port, $user = null, $password = null, $scheme = null)
+    public function __construct($ip, $port, $type = null, $user = null, $password = null, $scheme = null)
     {
-        $this->ip = $ip;
-        $this->port = $port;
-        $this->user = $user;
+        $this->ip       = $ip;
+        $this->port     = $port;
+        $this->type     = $type;
+        $this->type     = $type;
+        $this->user     = $user;
         $this->password = $password;
-        $this->scheme = $scheme;
+        $this->scheme   = $scheme;
 
     }
 
@@ -34,6 +38,11 @@ class Proxy implements ProxyInterface
     public function getPort()
     {
         return $this->port;
+    }
+
+    public function getType()
+    {
+        return strtoupper($this->type);
     }
 
     public function getUser()
@@ -56,7 +65,7 @@ class Proxy implements ProxyInterface
         $proxyPieces = explode('@', $proxy);
         if (count($proxyPieces) == 2) {
             $authPieces = explode(':', $proxyPieces[0]);
-            if (count($authPieces)>2) {
+            if (count($authPieces) > 2) {
                 throw new Exception('Bad proxy string. Expected format: [user[:passsword]@]ip:port');
             }
             if (!isset($authPieces[1])) {
@@ -67,12 +76,12 @@ class Proxy implements ProxyInterface
                 throw new Exception('Bad proxy string. Expected format: [user[:passsword]@]ip:port');
             }
         } elseif (count($proxyPieces) == 1) {
-            $authPieces = [null,null];
+            $authPieces = [null, null];
             $hostPieces = explode(':', $proxyPieces[0]);
         } else {
             throw new Exception('Bad proxy string. Expected format: [user[:passsword]@]ip:port');
         }
-        $options['login'] = $authPieces[0];
+        $options['login']    = $authPieces[0];
         $options['password'] = $authPieces[1];
         return new self($hostPieces[0], $hostPieces[1], $authPieces[0], $authPieces[1]);
     }
@@ -88,7 +97,7 @@ class Proxy implements ProxyInterface
         }
 
         if ($this->scheme) {
-            $proxy =  $this->scheme . '://' . $proxy;
+            $proxy = $this->scheme . '://' . $proxy;
         }
 
         return $proxy;
