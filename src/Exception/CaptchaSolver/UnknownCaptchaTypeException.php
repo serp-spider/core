d55c84863475773f32a21873f89918765a34c3e4
sghzal <sghzal@gmail.com>
2016-06-16 12:09:56 +0200

    better structure for UnknownCaptchaTypeException

--- src/Exception/CaptchaSolver/UnknownCaptchaTypeException.php
+++ src/Exception/CaptchaSolver/UnknownCaptchaTypeException.php
@@ -6,6 +6,7 @@
 namespace Serps\Exception\CaptchaSolver;
 
 use Exception;
+use Serps\Core\Captcha\CaptchaSolverInterface;
 
 /**
  * Thrown when a @see CaptchaSolverInterface does not support
@@ -16,8 +17,16 @@ class UnknownCaptchaTypeException extends CaptchaNotSolvableException
 
     protected $captchaType;
 
-    public function __construct($cpatchaType, $message = '', $code = 0, Exception $previous = null)
+    public function __construct($captchaType, CaptchaSolverInterface $captchaSolver, $additionalMessage = null, $code = 0, Exception $previous = null)
     {
+        $this->captchaType = $captchaType;
+
+        $captchaSolverClass = get_class($captchaSolver);
+        $message =  "Captcha of type $captchaType is not solvable by $captchaSolverClass.";
+        if($additionalMessage){
+            $message .= ' ' . $additionalMessage;
+        }
+
         parent::__construct($message, $code, $previous);
     }
 
