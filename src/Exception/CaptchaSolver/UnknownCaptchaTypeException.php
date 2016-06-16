0bc5a4d67b72b32f594accc81b2842c1bb63805f
sghzal <sghzal@gmail.com>
2016-06-16 12:13:02 +0200

    psr-2

--- src/Exception/CaptchaSolver/UnknownCaptchaTypeException.php
+++ src/Exception/CaptchaSolver/UnknownCaptchaTypeException.php
@@ -17,13 +17,19 @@ class UnknownCaptchaTypeException extends CaptchaNotSolvableException
 
     protected $captchaType;
 
-    public function __construct($captchaType, CaptchaSolverInterface $captchaSolver, $additionalMessage = null, $code = 0, Exception $previous = null)
-    {
+    public function __construct(
+        $captchaType,
+        CaptchaSolverInterface $captchaSolver,
+        $additionalMessage = null,
+        $code = 0,
+        Exception $previous = null
+    ) {
+    
         $this->captchaType = $captchaType;
 
         $captchaSolverClass = get_class($captchaSolver);
         $message =  "Captcha of type $captchaType is not solvable by $captchaSolverClass.";
-        if($additionalMessage){
+        if ($additionalMessage) {
             $message .= ' ' . $additionalMessage;
         }
 
