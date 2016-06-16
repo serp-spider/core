2858038e4682694fff2cbe4ab0506ffb5fda4ffd
sghzal <sghzal@gmail.com>
2016-06-16 12:42:03 +0200

    add captcha solving utility

---
+++ src/Core/Captcha/CaptchaSolving.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @license see LICENSE
+ */
+
+namespace Serps\Core\Captcha;
+
+/**
+ * this is an utility that helps to wait for a captcha to be solved
+ */
+class CaptchaSolving
+{
+
+    protected $getCaptha;
+
+    public function __construct(callable $getCaptcha)
+    {
+        $this->getCaptha = $getCaptcha;
+    }
+
+    public function getCaptcha()
+    {
+        return call_user_func($this->getCaptha);
+    }
+
+    /**
+     * try to get the captcha for the given time
+     * @param int $time max time to wait in second
+     * @param int $interval interval between 2 test in micro second
+     * @return null|mixed
+     */
+    public function tryFor($time, $interval = 200)
+    {
+        $tryUntil = time() + $time;
+        while ($tryUntil > time()) {
+            if ($c = $this->getCaptcha()) {
+                return $c;
+            }
+            usleep($interval);
+        }
+        return null;
+    }
+}
