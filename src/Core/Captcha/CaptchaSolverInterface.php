c53ce52a5481fa1a8711b439e62e9c176c05ebd4
sghzal <sghzal@gmail.com>
2016-07-01 09:12:52 +0200

    async solver

--- src/Core/Captcha/CaptchaSolverInterface.php
+++ src/Core/Captcha/CaptchaSolverInterface.php
@@ -11,10 +11,20 @@ use Serps\Exception\CaptchaSolver\UnknownCaptchaTypeException;
 interface CaptchaSolverInterface
 {
     /**
+     * Solve a captcha and returns data depending on the captcha type
      * @param CaptchaResponse $captchaResponse
      * @return mixed
      * @throws CaptchaNotSolvableException
      * @throws UnknownCaptchaTypeException
      */
     public function solve(CaptchaResponse $captchaResponse);
+
+    /**
+     * Submit a captcha for resolution and returns a helper to get the output on an async way
+     * @param CaptchaResponse $captchaResponse
+     * @return CaptchaSolving
+     * @throws CaptchaNotSolvableException
+     * @throws UnknownCaptchaTypeException
+     */
+    public function solveAsync(CaptchaResponse $captchaResponse);
 }
