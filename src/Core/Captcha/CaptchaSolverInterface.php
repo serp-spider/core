cb34f706e8736eeb27bdf115bd2cb56492448395
Soufiane Ghzal <sghzal@gmail.com>
2016-07-04 07:07:04 +0200

    Update CaptchaSolverInterface.php

--- src/Core/Captcha/CaptchaSolverInterface.php
+++ src/Core/Captcha/CaptchaSolverInterface.php
@@ -15,13 +15,14 @@ interface CaptchaSolverInterface
      * Solve a captcha and returns data depending on the captcha type
      * @param CaptchaResponse $captchaResponse
      * @param int $timeout the timeout for captcha resolution in milliseconds.
+     * @param int $tryDelay delay between each try of getting the captcha data.
      * Leave it null to use adapter's default value
      * @return mixed
      * @throws CaptchaNotSolvableException
      * @throws UnknownCaptchaTypeException
      * @throws TimeoutException
      */
-    public function solve(CaptchaResponse $captchaResponse, $timeout = null);
+    public function solve(CaptchaResponse $captchaResponse, $timeout = null, $tryDelay = null);
 
     /**
      * Submit a captcha for resolution and returns a helper to get the output on an async way
