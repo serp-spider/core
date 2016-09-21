ec15544c78a966ed519f53dc6b99f28083730753
Soufiane Ghzal <sghzal@gmail.com>
2016-09-21 11:22:06 +0200

    fix hhvm unit tests

--- test/suites/TDD/Core/Captcha/AsyncCaptchaSolvingCallbackTest.php
+++ test/suites/TDD/Core/Captcha/AsyncCaptchaSolvingCallbackTest.php
@@ -35,22 +35,24 @@ class AsyncCaptchaSolvingCallbackTest extends \PHPUnit_Framework_TestCase
 
     public function testCaptchaSolvingCallbackTryFor()
     {
-        $count = 0;
+        // Use std class to fix hhvm no taking references for lambdas captured variables
+        $count = new \stdClass();
+        $count->count = 0;
 
-        $asyncSolving = new AsyncCaptchaSolvingCallback(function () use (&$count) {
-            $count++;
-            return $count > 2;
+        $asyncSolving = new AsyncCaptchaSolvingCallback(function () use ($count) {
+            $count->count++;
+            return $count->count > 2;
         });
 
-        $this->assertEquals(0, $count);
+        $this->assertEquals(0, $count->count);
 
         $this->assertEquals(false, $asyncSolving->tryFor(1, 1000));
-        $this->assertEquals(1, $count);
+        $this->assertEquals(1, $count->count);
         $this->assertEquals(true, $asyncSolving->tryFor(10, 50));
-        $this->assertEquals(3, $count);
+        $this->assertEquals(3, $count->count);
 
         // Cached
         $this->assertEquals(true, $asyncSolving->tryFor(1, 1000));
-        $this->assertEquals(3, $count);
+        $this->assertEquals(3, $count->count);
     }
 }
