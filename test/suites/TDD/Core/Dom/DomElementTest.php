f9275ee4cb806a5800bac8e605410ac821464278
Soufiane Ghzal <sghzal@gmail.com>
2017-12-10 20:30:31 +0100

    missing tests

--- test/suites/TDD/Core/Dom/DomElementTest.php
+++ test/suites/TDD/Core/Dom/DomElementTest.php
@@ -33,6 +33,35 @@ class DomElementTest extends TestCase
         $this->assertTrue($foobar->hasClass('foo'));
         $this->assertTrue($foobar->hasClass('bar'));
         $this->assertFalse($foobar->hasClass('vaz'));
+
+        $noClass = $nodeList->item(1);
+        $this->assertFalse($noClass->hasClass('foo'));
+    }
+
+    public function testTagName()
+    {
+        $dom = new DocumentWrapper('<html><div>foo</div><span class="foo bar">baz</span><span>qux</span></html>');
+
+        $xpath = new \DOMXPath($dom->getDom());
+        $elements = $xpath->query(Css::toXPath('span'));
+        $nodeList = new DomNodeList($elements, $dom);
+
+        $foobar = $nodeList->item(0);
+
+        $this->assertEquals('span', $foobar->getTagName());
+    }
+
+    public function testGetNodeValue()
+    {
+        $dom = new DocumentWrapper('<html><div>foo</div><span class="foo bar">baz</span><span>qux</span></html>');
+
+        $xpath = new \DOMXPath($dom->getDom());
+        $elements = $xpath->query(Css::toXPath('span'));
+        $nodeList = new DomNodeList($elements, $dom);
+
+        $foobar = $nodeList->item(0);
+
+        $this->assertEquals('baz', $foobar->getNodeValue());
     }
 
     public function testHasClasses()
@@ -51,5 +80,8 @@ class DomElementTest extends TestCase
         $this->assertTrue($foobar->hasClasses(['foo', 'bar']));
         $this->assertFalse($foobar->hasClasses(['foo', 'baz']));
         $this->assertFalse($foobar->hasClasses(['baz']));
+
+        $noClass = $nodeList->item(1);
+        $this->assertFalse($noClass->hasClasses(['foo']));
     }
 }
